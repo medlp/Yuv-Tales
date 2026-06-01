cb82583789ccdd151d89d67cb2cd4b82655207b7
Médérick Leprince Gasser <mleprincegasser@gaming.tech>
2026-06-01 16:58:18 +0200

    Thanks To Tchoupi

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine.InputSystem;
 public class PlayerControllerTPS : MonoBehaviour
 {
     [Header("Movement Settings")]
-    public float walkSpeed = 7f;
-    public float sprintSpeed = 16.0f;
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 15.0f;
     public float rotationSpeed = 10f;
-    
 
     [Header("Momentum Settings")]
-    public float acceleration = 4f;   
-    public float deceleration = 4f; 
+    public float acceleration = 5f;
+    public float deceleration = 10f;
+
+    [Header("Slide Settings")]
+    public float slideInitialSpeed = 18f;
+    public float slideDeceleration = 4f;
+    public float minSpeedToSlide = 10f;
 
     [Header("Jump Settings")]
     public float jumpHeight = 1.2f;
@@ -23,9 +27,11 @@ public class PlayerControllerTPS : MonoBehaviour
     public float crouchSpeed = 2.5f;
     private float crouchHeight = 1.0f;
     private Vector3 crouchCenter = new Vector3(0, 0.5f, 0);
-     
+
+    // Variables privées
     private bool isSprinting;
-    private bool isCrouching = false;
+    [SerializeField] private bool isCrouching = false;
+    [SerializeField] private bool isSliding = false;
     private float originalHeight;
     private Vector3 originalCenter;
 
@@ -34,10 +40,8 @@ public class PlayerControllerTPS : MonoBehaviour
     private float yVelocity;
     private Transform cameraTransform;
     private bool isJumpPressed;
-     
 
-    private float speedTarget = 0f;
-    private Vector3 currentVelocityXZ;
+    [SerializeField] private Vector3 currentVelocityXZ;
 
     Animator animator;
 
@@ -82,11 +86,6 @@ public class PlayerControllerTPS : MonoBehaviour
         {
             isJumpPressed = true;
         }
-
-        if (context.canceled)
-        {
-            isJumpPressed = false;
-        }
     }
 
     public void OnSprint(InputAction.CallbackContext context)
@@ -106,32 +105,53 @@ public class PlayerControllerTPS : MonoBehaviour
     {
         if (context.performed)
         {
-            if (isCrouching)
+            if (currentVelocityXZ.magnitude >= minSpeedToSlide && isCrouching == false)
             {
+                isSliding = true;
+                isCrouching = true;
+                isSprinting = false;
+
+                Vector3 slideDirection = currentVelocityXZ.normalized;
+
+                if (slideDirection == Vector3.zero)
+                {
+                    slideDirection = transform.forward;
+                }
+
+                currentVelocityXZ = slideDirection * slideInitialSpeed;
+
+                animator.SetBool("IsSliding", true);
+                animator.SetBool("IsCrouching", false);
+
+                controller.height = crouchHeight;
+                controller.center = crouchCenter;
+                return;
+            }
+
+            if (isCrouching || isSliding)
+            {
+                isSliding = false;
                 isCrouching = false;
+
+                animator.SetBool("IsSliding", false);
+                animator.SetBool("IsCrouching", false);
+
+                controller.height = originalHeight;
+                controller.center = originalCenter;
             }
             else
             {
                 isCrouching = true;
-            }
+                animator.SetBool("IsCrouching", true);
 
-            if (isCrouching)
-            {
                 controller.height = crouchHeight;
                 controller.center = crouchCenter;
             }
-            else
-            {
-                controller.height = originalHeight;
-                controller.center = originalCenter;
-            }
-
-            animator.SetBool("IsCrouching", isCrouching);
         }
     }
 
     private void HandleMovement()
-    { 
+    {
         Vector3 camForward = cameraTransform.forward;
         camForward.y = 0f;
         camForward.Normalize();
@@ -141,25 +161,55 @@ public class PlayerControllerTPS : MonoBehaviour
         camRight.Normalize();
 
         Vector3 inputDirection = camRight * moveInput.x + camForward * moveInput.y;
-         
-        if (inputDirection != Vector3.zero)
+        float speedTarget = 0f;
+        Vector3 targetVelocityXZ = Vector3.zero;
+
+        if (isSliding)
         {
-            if (isCrouching)
+            targetVelocityXZ = Vector3.zero;
+
+            if (currentVelocityXZ.magnitude <= 0.5f)
             {
-                speedTarget = crouchSpeed;
-            }
-            else if (isSprinting)
-            {
-                speedTarget = sprintSpeed;
-            }
-            else
-            {
-                speedTarget = walkSpeed;
+                isSliding = false;
+                animator.SetBool("IsSliding", false);
+                currentVelocityXZ = Vector3.zero;
+
+                if (isCrouching)
+                {
+                    animator.SetBool("IsCrouching", true);
+                }
+                else
+                {
+                    animator.SetBool("IsCrouching", false);
+                }
             }
         }
-         
-        Vector3 targetVelocityXZ = inputDirection * speedTarget;
-         
+        else
+        {
+            if (inputDirection != Vector3.zero)
+            {
+                if (isCrouching)
+                {
+                    speedTarget = crouchSpeed;
+                }
+                else if (isSprinting)
+                {
+                    speedTarget = sprintSpeed;
+                }
+                else
+                {
+                    speedTarget = walkSpeed;
+                }
+            }
+
+            targetVelocityXZ = inputDirection * speedTarget;
+        }
+
+        if (isSliding)
+        {
+            currentVelocityXZ = Vector3.MoveTowards(currentVelocityXZ, targetVelocityXZ, slideDeceleration * Time.deltaTime);
+        }
+
         if (controller.isGrounded)
         {
             animator.SetBool("IsGrounded", true);
@@ -168,24 +218,34 @@ public class PlayerControllerTPS : MonoBehaviour
             {
                 yVelocity = -5f;
             }
-             
-            if (inputDirection != Vector3.zero)
-            { 
-                currentVelocityXZ = Vector3.Lerp(currentVelocityXZ, targetVelocityXZ, acceleration * Time.deltaTime);
-            }
-            else
-            { 
-                currentVelocityXZ = Vector3.Lerp(currentVelocityXZ, Vector3.zero, deceleration * Time.deltaTime);
-            }
-             
-            if (isJumpPressed && isCrouching == false)
+
+            if (isSliding == false)
             {
-                if (jumpTimer <= 0f)
+                if (inputDirection != Vector3.zero)
                 {
-                    yVelocity = Mathf.Sqrt(jumpHeight * -2f * -9.81f);
-                    animator.SetTrigger("Jump");
-                    jumpTimer = jumpCooldown;
+                    currentVelocityXZ = Vector3.Lerp(currentVelocityXZ, targetVelocityXZ, acceleration * Time.deltaTime);
                 }
+                else
+                {
+                    currentVelocityXZ = Vector3.Lerp(currentVelocityXZ, Vector3.zero, deceleration * Time.deltaTime);
+                }
+            }
+
+            if (isJumpPressed)
+            {
+                if (isCrouching == false)
+                {
+                    if (isSliding == false)
+                    {
+                        if (jumpTimer <= 0f)
+                        {
+                            yVelocity = Mathf.Sqrt(jumpHeight * -2f * -9.81f);
+                            animator.SetTrigger("Jump");
+                            jumpTimer = jumpCooldown;
+                        }
+                    }
+                }
+
                 isJumpPressed = false;
             }
         }
@@ -193,21 +253,37 @@ public class PlayerControllerTPS : MonoBehaviour
         {
             animator.SetBool("IsGrounded", false);
             yVelocity += -9.81f * Time.deltaTime;
-             
+
+            if (isSliding == false)
+            {
+                if (inputDirection != Vector3.zero)
+                {
+                    currentVelocityXZ = Vector3.Lerp(currentVelocityXZ, targetVelocityXZ, acceleration * 0.5f * Time.deltaTime);
+                }
+            }
         }
-         
+
         Vector3 finalVelocity = currentVelocityXZ;
         finalVelocity.y = yVelocity;
 
         controller.Move(finalVelocity * Time.deltaTime);
-         
+
         if (inputDirection != Vector3.zero)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(inputDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            if (isSliding == false)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(inputDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
         }
-         
+
         float currentSpeedForAnimator = currentVelocityXZ.magnitude;
+
+        if (isSliding)
+        {
+            currentSpeedForAnimator = 0f;
+        }
+
         animator.SetFloat("Speed", currentSpeedForAnimator, 0.2f, Time.deltaTime);
     }
 
