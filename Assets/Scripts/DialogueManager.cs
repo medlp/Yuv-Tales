81e6720cfc8d3a9d112c7126b6c55301bd273856
Loïc GARREL <lgarrel@gaming.tech>
2026-06-10 11:22:40 +0200

    WIP

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -98,8 +98,6 @@ public class DialogueManager : MonoBehaviour
         GameObject btn = Instantiate(choiceButtonPrefab, choicesPanel.transform);
         Vector2 btnPosition = choicesPanel.GetComponentInChildren<Transform>().transform.position;
 
-        Debug.Log(btnPosition.y);
-
         btn.GetComponentInChildren<TMP_Text>().text = text;
         btn.GetComponent<Button>().onClick.AddListener(() =>
         {
