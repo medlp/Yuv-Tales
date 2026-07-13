538e78691dcee2ae204e3062cd70192aa118b1c6
lgarrel <lgarrel@gaming.tech>
2026-07-13 15:57:56 +0200

    Start mini script dialogue

---
+++ Assets/Scripts/PNJ_ITEM/PlayerAwake.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerAwake : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private DialogueTrigger dialogueTrigger;
+
+    [Header("Parametres")]
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private bool destroyAfterTrigger = true;
+
+    private Collider zoneCollider;
+    private bool hasTriggered;
+
+    private void Awake()
+    {
+        zoneCollider = GetComponent<Collider>();
+        zoneCollider.isTrigger = true;
+
+        if (dialogueTrigger == null)
+            dialogueTrigger = GetComponent<DialogueTrigger>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CheckAlreadyInsideAtStart());
+    }
+
+    private IEnumerator CheckAlreadyInsideAtStart()
+    {
+        // Laisse la physique se stabiliser (colliders/rigidbodies pas encore
+        // "enregistrés" au tout premier frame lors d'un chargement de scène).
+        yield return new WaitForFixedUpdate();
+
+        if (hasTriggered) yield break;
+
+        Bounds bounds = zoneCollider.bounds;
+        Collider[] overlaps = Physics.OverlapSphere(bounds.center, bounds.extents.x);
+
+        foreach (Collider other in overlaps)
+        {
+            if (other.CompareTag(playerTag))
+            {
+                TriggerDialogue();
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered) return;
+        if (!other.CompareTag(playerTag)) return;
+
+        TriggerDialogue();
+    }
+
+    private void TriggerDialogue()
+    {
+        hasTriggered = true;
+        dialogueTrigger.StartDialogue();
+
+        if (destroyAfterTrigger)
+            Destroy(gameObject);
+    }
+}
