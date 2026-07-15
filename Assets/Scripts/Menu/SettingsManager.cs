69f3f7d9625859ab81063177bc301bc1916d79b8
Loïc GARREL <lgarrel@gaming.tech>
2026-07-15 15:02:08 +0200

    Probleme regler

--- Assets/Scripts/Menu/SettingsManager.cs
+++ Assets/Scripts/Menu/SettingsManager.cs
@@ -65,7 +65,7 @@ public class SettingsManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
